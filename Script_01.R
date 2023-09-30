a9665f869007ac5b07c6c50d5b094262fef9e69e
EliasPreza <48401643+eliaspreza@users.noreply.github.com>
2023-09-29 20:42:20 -0600

    gfd
    gdfgdf

---
+++ Script_01.R
@@ -0,0 +1,52 @@
+
+#/////////////////////////////////////////////////////////////////////////////////////////////////
+#------------------------------Script-01----------------------------------------------------------
+#------------------------------Análisis Demográfico-----------------------------------------------
+#/////////////////////////////////////////////////////////////////////////////////////////////////
+#---Por Elias Preza
+
+#=======================================================================StartChunk
+#--------------------------Librerias
+library(tidyverse)#---Wrangling data
+library(sjmisc)#---Frecuencias
+library(haven)#---Abrir base en formatos: SPSS,STATA y SAS
+library(codebook)#--Uso de frecuencias
+library(githubinstall)
+
+#--------------------------Ayudas Librerias
+?tidyverse
+?sjmisc
+?haven
+?codebook
+#=======================================================================EndChunk
+
+#=======================================================================StartChunk
+#--------------------------Abriendo y explorando la EHPM
+
+EHPM<-read_sav("Bases/EHPM 2022.sav")#---Se lee EHPM de datos
+#Dic_00<-codebook_table(EHPM)#---Construyendo el diccionario
+#write_excel_csv(Dic_00,"EHPMs/Diccionario.csv")#---Se guarda diccionario
+Dic <- read_csv("Bases/Diccionario.csv")#---Se lee diccionario
+View(Dic)#---Ver Diccionario
+View(EHPM)#---Ver EHPM de datos
+
+#------------------------Explorando el contenido de la EHPM
+
+dplyr::glimpse(EHPM) #---Estructura del dataframe
+head(EHPM,5) #-----Visualización
+tail(EHPM,20) #-----Visualización
+nrow(EHPM) #---número de lineas o casos
+ncol(EHPM) #---número de columnas o variables
+dim(EHPM) #---Dimension de la EHPM: lineas - columnas
+length(EHPM) #---número de columnas o variables
+colnames(EHPM) #----Nombre de las columnas
+names(EHPM) #----Nombre de las columnas
+str(EHPM) #--Estructura del dataframe
+
+#------------------------Explorando variables
+
+str(EHPM$region) #--Estructura de la variable
+dplyr::glimpse(EHPM$region) #--Estructura de la variable
+
+#=======================================================================EndChunk
+
